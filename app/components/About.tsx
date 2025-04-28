e8ec088d9885e2afaafce7e1476685705a4d303e
bhumikaPandeyCodes <140793724+bhumikaPandeyCodes@users.noreply.github.com>
2025-04-28 14:51:44 +0530

    removed '

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -8,7 +8,7 @@ export function About(){
                 <p className="relative text-white flex flex-wrap max-w-[520px] my-4 text-sm md:text-md pt-2 pb-4">
                     I am a full stack developer skilled in building scalable web applications from India. 
                     <br/><br/>
-                    I have completed my Bachelor's of Computer Application from GGSIPU.
+                    I have completed my Bachelors of Computer Application from GGSIPU.
                     <br/><br/>
                     I specialize in turning ideas into fully functional web applications and pixel-perfect user interfaces. I can help bring your vision to life with clean, efficient code.
                     <span className="absolute inset-x-0 bottom-0 bg-gradient-to-r from-transparent via-gray-100 to-transparent h-[1px] w-3/4 mx-auto"></span>
