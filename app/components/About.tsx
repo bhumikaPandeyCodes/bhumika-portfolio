85390710b8385e8ad8a8d610b23a99c2fca8a51b
BhumiCodes <140793724+bhumikaPandeyCodes@users.noreply.github.com>
2025-09-07 18:31:35 +0530

    Update About.tsx

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -6,11 +6,11 @@ export function About(){
             <div className="flex flex-col justify-center items-center">
                 <Title title="About"/>
                 <p className="relative text-white flex flex-wrap max-w-[520px] my-4 text-sm md:text-base pt-2 pb-4">
-                    I am a full stack developer skilled in building scalable web applications from India. 
+                    I am a full stack developer from India, skilled in building scalable web applications.
                     <br/><br/>
-                    I have completed my Bachelor&apos;s of Computer Application from GGSIPU.
+                    Currently, I am working full-time at CMTAI, where for the past 3 months, I have been developing a variety of full-stack projects for clients. I specialize in turning ideas into fully functional web applications and pixel-perfect user interfaces. I can help bring your vision to life with clean, efficient code.
                     <br/><br/>
-                    I specialize in turning ideas into fully functional web applications and pixel-perfect user interfaces. I can help bring your vision to life with clean, efficient code.
+                    I have completed my Bachelor's of Computer Application from GGSIPU.
                     <span className="absolute inset-x-0 bottom-0 bg-gradient-to-r from-transparent via-gray-100 to-transparent h-[1px] w-3/4 mx-auto"></span>
                 </p>
             </div>
