353e455c8aa5ae06e48a362ce1bc0e8c54fbb8aa
BhumiCodes <140793724+bhumikaPandeyCodes@users.noreply.github.com>
2025-09-07 18:32:31 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Projects from "./components/Projects";
 import { About } from "./components/About";
 import { Skills } from "./components/Skills";
 import { Footer } from "./components/Footer";
+import { Experience } from "./components/Experience";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
         <Navbar />
         <Hero />
         <About />
+        <Experience />
         <Projects />
         <Skills/>
         <Footer />
