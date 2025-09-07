173ee61b6a83bdcac53ce613faa46d8f43b1835b
BhumiCodes <140793724+bhumikaPandeyCodes@users.noreply.github.com>
2025-09-07 18:31:03 +0530

    Update Skills.tsx

--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -1,7 +1,7 @@
 import { Tag } from "../ui/tag"
 
 export function Skills(){
-    const skills = ["HTML","CSS","Javascript","Bootstrap","Tailwind","Typescript","Reactjs","Nodejs", "Expressjs", "Mongodb", "Mongoose","Postgres","Prisma"]
+    const skills = ["HTML","CSS","Javascript","Bootstrap","Tailwind","Typescript","React.js", "Next.js", "Node.js", "Express.js", "Mongodb", "Mongoose","Postgres","Prisma", "s3", "cloudinary"]
     return(
         <div className="h-screen flex flex-col justify-center items-center " id="Skills">
                 <div className=" py-1  bg-clip-text text-transparent bg-gradient-to-b from-white to-gray-400 ">
